46269f172b83c9e8722f63eab704789ce36d2430
THUxzy <xincy22@mails.tsinghua.edu.cn>
2024-12-18 15:03:34 +0800

    student 1.1 Two student models were set up, one to fit the timing and one to fit the outputs

---
+++ student/__init__.py
@@ -0,0 +1,4 @@
+from . import output as OUTPUT
+from . import time_series as SERIES
+
+__all__ = ['OUTPUT', 'SERIES']
---
+++ student/output/__init__.py
@@ -0,0 +1,3 @@
+from .models import DeepMLP, DeepResNet
+
+__all__ = ['DeepMLP', 'DeepResNet']
