005d547aeb4553306841c884c7c4b884dcec4118
xzy-asus <xincy22@mails.tsinghua.edu.cn>
2024-12-19 15:34:56 +0800

    train & test 1.1

---
+++ data/distill_dataset/__init__.py
@@ -0,0 +1,3 @@
+from .data_manager import DistillDataManager
+
+__all__ = ['DistillDataManager']
--- student/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import output as OUTPUT
-from . import time_series as SERIES
-
-__all__ = ['OUTPUT', 'SERIES']
